fe1a42221f72969d191d3029588747787fbc8ac4
alex1331 <danilin999@bk.ru>
2023-06-06 13:15:49 +0300

    Переделанная задача

--- Modul_2/HW_4/Pr_2/HW_2.py
+++ Modul_2/HW_4/Pr_2/HW_2.py
@@ -8,36 +8,35 @@
 
 class Solution:
     def findMinIndexSum(self, list1, list2):
-        common_strings = {}  # словарь для хранения общих строк и их сумм индексов
+        common_strings = {}
 
-        # заполнение словаря значениями из list1
-        for i in range(len(list1)):
-            common_strings[list1[i]] = i
+        for i, string in enumerate(list1):
+            common_strings[string] = i
 
-        # обновление словаря значениями из list2 и вычисление сумм индексов
-        for j in range(len(list2)):
-            if list2[j] in common_strings:
-                common_strings[list2[j]] += j
-            else:
-                common_strings[list2[j]] = j
+        min_index_sum = float('inf')
+        result = []
 
-        min_index_sum = min(common_strings.values())  # нахождение минимальной суммы индексов
-
-        result = [string for string, index_sum in common_strings.items() if index_sum == min_index_sum]
+        for j, string in enumerate(list2):
+            if string in common_strings:
+                index_sum = j + common_strings[string]
+                if index_sum < min_index_sum:
+                    min_index_sum = index_sum
+                    result = [string]
+                elif index_sum == min_index_sum:
+                    result.append(string)
 
         return result
 
 
-# примеры использования
 list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]
 list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
 solution = Solution()
-print(solution.findMinIndexSum(list1, list2))  # Output: ["Shogun"]
+print(solution.findMinIndexSum(list1, list2))
 
 list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]
 list2 = ["KFC", "Shogun", "Burger King"]
-print(solution.findMinIndexSum(list1, list2))  # Output: ["Shogun"]
+print(solution.findMinIndexSum(list1, list2))
 
 list1 = ["happy", "sad", "good"]
 list2 = ["sad", "happy", "good"]
-print(solution.findMinIndexSum(list1, list2))  # Output: ["sad", "happy"]
+print(solution.findMinIndexSum(list1, list2))
