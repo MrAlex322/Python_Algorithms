acbb31ed0c8832b461bb9d1c7d6e51f5c17269f0
alex1331 <danilin999@bk.ru>
2023-06-18 18:39:45 +0300

    Исправлены имена переменных i,j,string

--- Modul_2/HW_4/Pr_2/HW_2.py
+++ Modul_2/HW_4/Pr_2/HW_2.py
@@ -4,26 +4,27 @@
 # в списках list1[i] и list2[j], то i + j должно быть минимальным значением среди всех остальных общих строк.
 # Возвращает все общие строки с наименьшей суммой индексов. Верните ответ в любом порядке.
 # TimeComplexity - O(N), где N - максимальная длина из двух списков list1 и list2.
+# Memory complexity - (len(list1))
 
 
 class Solution:
     def findMinIndexSum(self, list1, list2):
         common_strings = {}
 
-        for i, string in enumerate(list1):
-            common_strings[string] = i
+        for i_el, element in enumerate(list1):
+            common_strings[element] = i_el
 
         min_index_sum = float('inf')
         result = []
 
-        for j, string in enumerate(list2):
-            if string in common_strings:
-                index_sum = j + common_strings[string]
+        for j_el, element in enumerate(list2):
+            if element in common_strings:
+                index_sum = j_el + common_strings[element]
                 if index_sum < min_index_sum:
                     min_index_sum = index_sum
-                    result = [string]
+                    result = [element]
                 elif index_sum == min_index_sum:
-                    result.append(string)
+                    result.append(element)
 
         return result
 
