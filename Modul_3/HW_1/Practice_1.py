4178aaf23723468fd34fe2fca30ab71f3ae07fc5
alex1331 <danilin999@bk.ru>
2023-06-19 12:57:39 +0300

    Добавлено решение задачи 1 недели 3 месяца. Задача рещена с помощью бинарного поиска.

---
+++ Modul_3/HW_1/Practice_1.py
@@ -0,0 +1,37 @@
+# Даны массив arr, target число
+# Известно, что массив отсортирован в порядке возрастания. вернуть Нужно найти индекс, если target найден в arr.
+# Если нет, вернуть индекс туда, где он был бы, если бы он был вставлен по порядку.
+# Примеры:
+# arr = [1,3,5,6], target = 5
+# Ответ: 2
+# arr = [1,3,5,6], target = 2
+# Ответ: 1
+
+class BinarySearch:
+    def search_insert(self, arr, target):
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+
+
+# Пример использования
+arr = [2, 8, 9, 11]
+target = 9
+
+bs = BinarySearch()
+result = bs.search_insert(arr, target)
+print(result)
+
+
+
